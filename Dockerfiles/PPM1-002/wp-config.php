9ef30fd80e4ec089a6716112da6987e156467286
yasmine <67557535+yasmine2001ab@users.noreply.github.com>
2024-05-24 09:53:24 +0200

    Update wp-config.php

--- Dockerfiles/PPM1-002/wp-config.php
+++ Dockerfiles/PPM1-002/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', '${DB_NAME}' );
 
 /** Database username */
-define( 'DB_USER', 'wordpress' );
+define( 'DB_USER', '${DB_USER}' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'test' );
+define( 'DB_PASSWORD', '${DB_PASSWORD}' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
