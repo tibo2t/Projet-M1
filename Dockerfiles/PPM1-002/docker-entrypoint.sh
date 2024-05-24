d7ba555a2f6cca4eb0ab76e7025d5143da8ae455
yasmine <67557535+yasmine2001ab@users.noreply.github.com>
2024-05-24 10:42:54 +0200

    Update docker-entrypoint.sh

--- Dockerfiles/PPM1-002/docker-entrypoint.sh
+++ Dockerfiles/PPM1-002/docker-entrypoint.sh
@@ -6,6 +6,7 @@ if [ -f /var/www/html/wp-config.php ]; then
     sed -i "s/\${DB_NAME}/${DB_NAME}/g" /var/www/html/wp-config.php
     sed -i "s/\${DB_USER}/${DB_USER}/g" /var/www/html/wp-config.php
     sed -i "s/\${DB_PASSWORD}/${DB_PASSWORD}/g" /var/www/html/wp-config.php
+    sed -i "s/\${DB_HOST}/${DB_HOST}/g" /var/www/html/wp-config.php
 
 fi
 exec apachectl -D FOREGROUND
