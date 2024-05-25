4090e39332308d42ece312d51f6a670add18206c
adembaransdv <155978181+adembaransdv@users.noreply.github.com>
2024-05-25 13:01:29 +0200

    Add files via upload

---
+++ docker-compose/wp/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Remplacer les variables du fichier wp-config.php par les valeurs d'environnement
+if [ -f /var/www/html/wp-config.php ]; then
+    sed -i "s/\${DB_NAME}/${DB_NAME}/g" /var/www/html/wp-config.php
+    sed -i "s/\${DB_USER}/${DB_USER}/g" /var/www/html/wp-config.php
+    sed -i "s/\${DB_PASSWORD}/${DB_PASSWORD}/g" /var/www/html/wp-config.php
+    sed -i "s/\${DB_HOST}/${DB_HOST}/g" /var/www/html/wp-config.php
+
+fi
+exec apachectl -D FOREGROUND
