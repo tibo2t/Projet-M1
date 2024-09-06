3aba82154572d58d237519d726d46fae867b101f
Ubuntu <azureuser@projetM1.tzlf11rym54udmpddll0twraxb.parx.internal.cloudapp.net>
2024-09-06 21:15:59 +0000

    Modification du fichier docker-entrypoint.sh

--- Dockerfiles/PPM1-002/docker-entrypoint.sh
+++ Dockerfiles/PPM1-002/docker-entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+mysqld_safe &
+sleep 10  # Attendre que MariaDB soit complètement démarré
+
+# Exécuter les commandes pour créer la base de données et l'utilisateur
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+mysql -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
 set -e
 
 # Remplacer les variables du fichier wp-config.php par les valeurs d'environnement
