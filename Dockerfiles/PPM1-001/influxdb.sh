bfa9a687e17eabd0e3dc4d3a036dab464798fab3
yasmine <67557535+yasmine2001ab@users.noreply.github.com>
2024-09-27 16:05:28 +0200

    Update influxdb.sh

--- Dockerfiles/PPM1-001/influxdb.sh
+++ Dockerfiles/PPM1-001/influxdb.sh
@@ -1,17 +1,22 @@
- #!/bin/sh
+#!/bin/sh
 
+# Lancer InfluxDB en arrière-plan
 influxd &
 INFLUX_PID=$!
 
+# Attendre que InfluxDB démarre complètement
 sleep 10
 
-influx setup \
-  --username wordpress \
-  --password test12test \
-  --org wordpress \
-  --bucket wordpress \
-  --token wordpress \
-  --force
+# Vérifier si l'organisation et le bucket existent déjà
+if ! influx org list | grep -q "${INFLUX_ORG}"; then
+  influx setup \
+    --username "${INFLUX_USERNAME}" \
+    --password "${INFLUX_PASSWORD}" \
+    --org "${INFLUX_ORG}" \
+    --bucket "${INFLUX_BUCKET}" \
+    --token "${INFLUX_TOKEN}" \
+    --force
+fi
 
+# Attendre la fin du processus InfluxDB
 wait $INFLUX_PID
-
